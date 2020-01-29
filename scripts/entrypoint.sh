8fa565ed78ab798698c513192cf81b0bebed9e8b
mogluu <47318409+mogluu@users.noreply.github.com>
2020-01-29 03:09:44 -0500

    Generate keystore in the image

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -12,4 +12,13 @@ envsubst < $CONF/oxd-server-template.yml > $CONF/oxd-server.yml
 # entrypoint
 # ==========
 
+keytool -genkey -noprompt \
+  -alias oxd-server \
+  -dname "CN=oxd-server, OU=ID, O=Gluu, L=Gluu, S=TX, C=US" \
+  -keystore oxd-server.keystore \
+  -storepass $APPLICATION_KEYSTORE_PASSWORD \
+  -keypass $ADMIN_KEYSTORE_PASSWORD \
+  -deststoretype pkcs12 \
+  -keysize 2048 && mv oxd-server.keystore $CONF
+
 sh $BIN/oxd-start.sh
